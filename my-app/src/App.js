893737deb9eaf57d4cdaf90cb5c7fab27f462b5b
Rguardado1009 <rguardado1009@gmail.com>
2021-08-23 23:43:39 -0400

    fetch and render working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Home from "./Home";
+
 
 function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
-}
+	const [brands, setBrands] = useState([]);
+	const [questions, setQuestions] = useState([]);
 
+	useEffect(() => {
+		fetch("http://localhost:8000/brands")
+			.then((response) => response.json())
+			.then((brands) => setBrands(brands));
+	}, []);
+	useEffect(() => {
+		fetch("http://localhost:8000/Questions")
+			.then((response) => response.json())
+			.then((questions) => setQuestions(questions));
+	}, []);
+
+console.log(brands)
+console.log(questions)
+
+	return (
+		<div>
+			<Home brands={brands} questions={questions}/>
+            
+		</div>
+	);
+}
 export default App;
