4b19cfa6d146fba011c713afb27fbbbf36a398ea
Charles Lobe <lobecf@gmail.com>
2021-08-25 17:01:55 -0400

    Updated Cards

--- my-app/src/AdviceCard.js
+++ my-app/src/AdviceCard.js
@@ -1,53 +1,21 @@
 import React, { useState } from "react";
-import styled from "styled-components";
-
-
-
+import Comments from "./Comments";
 
 function AdviceCard({ brand }) {
 	const [likeBtn, setLikeBtn] = useState(0)
-	const [dislikeBtn, setDislikeBtn] = useState(0)
-
-	const [commentList, setCommentList] = useState([])
-	const [commentText, setCommentText] = useState("")
- 	
-	function addComment(){
-		return (
-			<div className="comments">
-				<p>{commentList}</p>
-			</div>
-		)
-	}
-	
-	function handleSubmit(event){
-		event.preventDefault()
-		setCommentList([...commentList, commentText])
-	}
-	
-	
+	const [dislikeBtn, setDislikeBtn] = useState(0)	
 	return (
 		<div className="advice-cards">
 			<h3>{brand.brand}</h3>
 			<img className="brand-images" src={brand.image} />
 			<p> Price Range: {brand.price}</p>
 			<p>Brand Type: {brand.type}</p>
+			<a href={brand.item_url}>Shop Now</a>
 			<span className="voteBtns">
                 <button onClick={() => setLikeBtn(likeBtn + 1)}>{likeBtn}  👍</button>
                 <button onClick={() => setDislikeBtn(dislikeBtn + 1)}>{dislikeBtn}  👎</button>
             </span>
-			<form onSubmit={handleSubmit}>
-				<input 
-				type="text" 
-				value={commentText} 
-				onChange={e => setCommentText(e.target.value)} 
-				placeholder="Leave a comment"/>
-				<input
-				type="submit"
-				value="Post"
-				/>
-			</form>
-			{addComment()}
-
+			<Comments/>
 		</div>
 	);
 }
